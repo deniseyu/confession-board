e0b41aa83050c6df170ed92856c9667bcc710815
Denise <yu.denise.d@gmail.com>
2014-10-09 20:20:29 +0100

    Added timestamps

--- lib/confession.rb
+++ lib/confession.rb
@@ -1,8 +1,15 @@
+require 'dm-timestamps'
+
 class Confession
 
 	include DataMapper::Resource
 
 	property :id, 			Serial
-	property :content,	String, :length =>140
+	property :content,	String, :length => 140
+  property :created_at, DateTime
+
+  # def timestamp
+  #   Time.now.strftime("Posted at %H:%M, %e %b %y")
+  # end
 
 end
