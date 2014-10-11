e7e0ab04ee962b010cb23f440a1e935d1cabea96
Denise <yu.denise.d@gmail.com>
2014-10-11 18:55:29 +0100

    Database issues... fingers crossed

--- app/datamapper_helper.rb
+++ app/datamapper_helper.rb
@@ -3,3 +3,6 @@ env = ENV["RACK_ENV"] || "development"
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/confession_board_#{env}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
+
+
+# DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/confession_board_#{env}")
